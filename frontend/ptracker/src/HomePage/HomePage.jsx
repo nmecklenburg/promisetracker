7e47422111cc928f19ec4bbfeb288b8ab5e0009c
menahassan <34526502+menahassan@users.noreply.github.com>
2025-02-14 13:37:15 -0800

    Update HomePage.jsx

--- frontend/ptracker/src/HomePage/HomePage.jsx
+++ frontend/ptracker/src/HomePage/HomePage.jsx
@@ -5,7 +5,6 @@ import api from '../api'
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
-
 const HomePage = () => {
   const { candidateId } = useParams();
 
