49d8e28a26e12b5c361ca1179d0734692bb204fa
menahassan <34526502+menahassan@users.noreply.github.com>
2025-02-14 13:35:21 -0800

    edits to make merging in hannah's branch easier

--- frontend/ptracker/src/HomePage/HomePage.jsx
+++ frontend/ptracker/src/HomePage/HomePage.jsx
@@ -1,12 +1,34 @@
 import React from "react";
 import CandidateProfile from "./CandidateProfile";
 import PromisesList from "./PromisesList";
+import api from '../api'
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
-const HomePage = ({ candidate }) => {
+
+const HomePage = () => {
+  const { candidateId } = useParams();
+
+  const [data, setData] = useState(null);
+
+  const fetchData = async () => {
+    try {
+      const response = await api.get(`/api/v1/candidates/${candidateId ? candidateId : 3}`); // have daniel lurie as default for now
+      console.log(response.data);
+      setData(response.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <>
-      <CandidateProfile candidate={candidate} />
-      <PromisesList candidate={candidate} />
+      <CandidateProfile candidate={data} />
+      <PromisesList candidate={data} />
     </>
   );
 };
